a98e3cc6a0a106cbb88768b9278b80d6ecbe9405
mugiwara <145732203+Phoenixixixi@users.noreply.github.com>
2025-10-29 22:02:43 +0700

    Delete module exports directory

--- module exports/import.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-// const moment = require('moment')
-const coba = require('./coba')
-
-coba.cetakNama('dustin')
-
-console.log(coba.mahasiswa.cetakMahasiswa())
-console.log(coba.PI)
-console.log(new coba.Orang())
